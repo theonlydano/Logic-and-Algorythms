99fdc5f1483ca412da9f0364f1e65a9571b78d9b
theonlydano <pichler.dano@outlook.com>
2026-02-01 16:32:15 +0100

    finish RDP without RDPException

--- 260201_RecursiveDescentParser/src/main/java/RDP.java
+++ 260201_RecursiveDescentParser/src/main/java/RDP.java
@@ -1,66 +1,91 @@
 public class RDP {
+
     private String string;
 
     public RDP(String s) {
         this.string = s.replaceAll(" ", "");
     }
 
-    public double evaluate() throws RDPExeption{
+    public double evaluate() throws RDPExeption {
         return evaluateIntern(string);
     }
 
-    public int find(String s, char c){
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == c){
-                return i;
-            }
-        }
-        return -1;
-    }
+    public double evaluateIntern(String s) throws RDPExeption {
 
-    public double evaluateIntern(String s) throws RDPExeption{
-        System.out.println(s);
-
-        double result = 0;
-
-        if(s.isEmpty()){
+        if (s.isEmpty()) {
             throw new RDPExeption("Empty string");
         }
-        if(s.charAt(0) == '+' || s.charAt(0) == '-'){
+
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
             s = "0" + s;
         }
 
-        int openedBracket = -1;
-        int closedBracket = -1;
+        double result = 0;
         char lastOperator = '+';
-        for(int i = 0; i < s.length(); i++){
-            System.out.println("Result: " + result);
-            System.out.println("String: " + s);
-            System.out.println("Char at: " + s.charAt(i));
-            switch(s.charAt(i)){
+
+        int bracketCount = 0;
+        int openedBracket = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            switch (c) {
+
                 case '(':
-                    openedBracket = i;
+                    if (bracketCount == 0) {
+                        openedBracket = i;
+                    }
+                    bracketCount++;
                     break;
+
                 case ')':
-                    closedBracket = i;
-                    result += evaluateIntern(s.substring(openedBracket + 1, closedBracket));
-                    String temp = s.substring(0, openedBracket) + s.substring(closedBracket, s.length() - closedBracket + 2);
-                    s = temp;
-                    break;
-                case '1', '2', '3', '4', '5', '6', '7', '8', '9':
-                    switch(lastOperator){
-                        case '+':
-                            result += Double.parseDouble(s.charAt(i) + "");
-                            break;
-                        case '-':
-                            result -= Double.parseDouble(s.charAt(i) + "");
-                            break;
+                    bracketCount--;
+
+                    if (bracketCount == 0) {
+
+                        double inner = evaluateIntern(s.substring(openedBracket + 1, i));
+
+                        s = s.substring(0, openedBracket) + inner + s.substring(i + 1);
+
+                        i = -1;
+                        lastOperator = '+';
+                        result = 0;
                     }
                     break;
-                case '-', '+':
-                    lastOperator = s.charAt(i);
+
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    if (bracketCount == 0) {
+                        lastOperator = c;
+                    }
+                    break;
+
+                default:
+
+                    if (bracketCount == 0 && (Character.isDigit(c))) {
+                        int start = i;
+
+                        while (i < s.length() && (Character.isDigit(s.charAt(i)) || s.charAt(i) == '.')) {
+                            i++;
+                        }
+
+                        double num = Double.parseDouble(s.substring(start, i));
+
+                        i--;
+
+                        switch (lastOperator) {
+                            case '+': result += num; break;
+                            case '-': result -= num; break;
+                            case '*': result *= num; break;
+                            case '/': result /= num; break;
+                        }
+                    }
             }
         }
+
         return result;
     }
 }
