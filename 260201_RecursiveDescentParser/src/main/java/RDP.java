b9b5ffb79bd2df2bee36a860583c9ad861d5bb26
theonlydano <pichler.dano@outlook.com>
2026-02-01 15:47:21 +0100

    work on RDP evaluate

--- 260201_RecursiveDescentParser/src/main/java/RDP.java
+++ 260201_RecursiveDescentParser/src/main/java/RDP.java
@@ -2,21 +2,47 @@ public class RDP {
     private String string;
 
     public RDP(String s) {
-        this.string = s;
+        this.string = s.replaceAll(" ", "");
     }
 
-    public double evaluate(String s) throws RDPExeption{
-        // Yet to be implemented
+    public double evaluate() throws RDPExeption{
+        double result = 0;
+
+
+
         return 0;
     }
 
     public int find(String s, char c){
-        // Yet to be implemented
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == c){
+                return i;
+            }
+        }
         return -1;
     }
 
     public double evaluateIntern(String s) throws RDPExeption{
-        // Yet to be implemented
+        double result = 0;
+
+        if(s.isEmpty()){
+            throw new RDPExeption();
+        }
+        if(s.charAt(0) == '+' || s.charAt(0) == '-'){
+            s = "0" + s;
+        }
+
+        int openedBracket = -1;
+        int closedBracket = -1;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                openedBracket = i;
+            }
+            if(s.charAt(i) == ')'){
+                closedBracket = i;
+                result += evaluateIntern(s.substring(openedBracket + 1, closedBracket));
+            }
+        }
         return 0;
     }
 }
--- 260201_RecursiveDescentParser/src/main/java/Tester.java
+++ 260201_RecursiveDescentParser/src/main/java/Tester.java
@@ -1,5 +1,15 @@
 public class Tester {
     public static void main(String[] args) {
+        RDP parser = new RDP("-((4+5)*2)*(3+1)");
+        /*
+            // These expressions should cause an exception...
+            RDParser parser = new RDParser("-((4+5)2)*(3+1)");
+            RDParser parser = new RDParser("-((4+5)*2*(3+1)");
+            RDParser parser = new RDParser("((4x+5)*2)*(3+1)");
+         */
+        try {
+            System.out.println(parser.evaluate());
+        } catch(RDPExeption e) { e.printStackTrace(); }
 
     }
 }
